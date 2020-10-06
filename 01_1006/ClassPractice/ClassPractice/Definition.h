0d2b1993b850a4866d936433c1794a285d8a47b8
MSI\mkdea <mikidea.1004@gmail.com>
2020-10-06 16:02:52 +0900

    リファクタリングその3 ・Unitに直接宣言していた変数を、構造体にまとめる ・定義をまとめておくためのDefinitionを作成

---
+++ 01_1006/ClassPractice/ClassPractice/Definition.h
@@ -0,0 +1,20 @@
+#ifndef DEFINITION_H
+#define DEFINITION_H
+
+
+//Unitの持つステータス構造体
+struct Status
+{
+	int _hp;
+	int _str;
+	int _mag;
+	int _tec;
+	int _spd;
+	int _def;
+	int _luc;
+	int _mde;
+};
+
+
+
+#endif
