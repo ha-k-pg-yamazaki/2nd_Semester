162672e933c2fcde93943845c60e34233ab46efe
MSI\mkdea <mikidea.1004@gmail.com>
2020-10-06 13:47:38 +0900

    リファクタリング前、ステータスを画面に表示する処理を追加

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -23,11 +23,41 @@ public:
 
 };
 
+Fighter::Fighter()
+{
+	m_hp = 0;
+	m_str = 0;
+	m_mag = 0;
+	m_tec = 0;
+	m_spd = 0;
+	m_def = 0;
+	m_luc = 0;
+	m_mde = 0;
+
+}
+
+Fighter::~Fighter()
+{
+
+}
 
 
 
 int main()
 {
+	Fighter Fighter;
+
+	printf("m_hp = %d\n", Fighter.m_hp);
+	printf("m_str = %d\n", Fighter.m_str);
+	printf("m_mag = %d\n", Fighter.m_mag);
+	printf("m_tec = %d\n", Fighter.m_tec);
+	printf("m_spd = %d\n", Fighter.m_spd);
+	printf("m_def = %d\n", Fighter.m_def);
+	printf("m_luc = %d\n", Fighter.m_luc);
+	printf("m_mde = %d\n", Fighter.m_mde);
+
+
+
 	system("pause");
 	return 0;
 }
