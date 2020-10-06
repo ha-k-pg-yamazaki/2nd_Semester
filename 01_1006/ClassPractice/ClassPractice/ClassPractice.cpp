589bc8a44f606923bc8e0bdcbcbed00586fc1cef
MSI\mkdea <mikidea.1004@gmail.com>
2020-10-06 13:18:16 +0900

    クラスの作り方の復習用プロジェクトの作成

---
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+
+//Fighterクラスを作成
+class Fighter
+{
+public:
+	//コンストラクタ
+	Fighter();
+	//デストラクタ
+	~Fighter();
+
+	int m_hp;
+	int m_str;
+	int m_mag;
+	int m_tec;
+	int m_spd;
+	int m_def;
+	int m_luc;
+	int m_mde;
+
+
+
+};
+
+
+
+
+int main()
+{
+	system("pause");
+	return 0;
+}
